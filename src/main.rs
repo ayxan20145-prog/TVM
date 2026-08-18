7eaecf11b64a29f0ebbfd429c483826703a44e30
ayxan <ayxan.20145@gmail.com>
2026-08-18 11:08:09 +0400

    create new vm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+enum Instruction {
+    Add,
+    Print,
+}
+
+struct VM {
+    stack: Vec<i32>,
+}
+
+impl VM {
+    fn new() {
+        let mut vm = VM { stack: Vec::new() };
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut vm = VM::new();
 }
